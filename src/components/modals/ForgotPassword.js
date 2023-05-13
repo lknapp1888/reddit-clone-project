a746fb939a9093a0ece106c6385a71bee6c292d8
Lewis Knapp <lknapp1888@gmail.com>
2023-05-13 17:57:25 +0100

    Add password reset functionallity

--- src/components/modals/ForgotPassword.js
+++ src/components/modals/ForgotPassword.js
@@ -1,52 +1,146 @@
 import {
-    Button,
-    FormControl,
-    Flex,
-    Heading,
-    Input,
-    Stack,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  
-  
-  export default function ForgotPassword() {
+  Button,
+  FormControl,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  useColorModeValue,
+  Spinner,
+} from "@chakra-ui/react";
+
+import { CheckIcon } from "@chakra-ui/icons";
+
+import { useState } from "react";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import { auth } from "../../config/firebase";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [sentStatus, setSentStatus] = useState(false)
+  const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
+
+
+  if (sentStatus) {
     return (
-      <Flex
+      <Flex align={"center"} justify={"center"} bg="gray.200">
+      <Stack
+        spacing={4}
+        w={"full"}
+        maxW={"md"}
+        bg="white"
+        rounded={"xl"}
+        boxShadow={"lg"}
+        p={6}
+        my={12}
         align={'center'}
-        justify={'center'}
-        bg='gray.200'>
+      >
+        <CheckIcon color='green' boxSize={6}></CheckIcon> 
+        <Text fontSize='xl'>Reset email sent</Text>
+      </Stack>
+    </Flex>
+    )
+  }
+  if (sending) {
+    return (
+      <Flex align={"center"} justify={"center"} bg="gray.200">
         <Stack
           spacing={4}
-          w={'full'}
-          maxW={'md'}
-          bg='white'
-          rounded={'xl'}
-          boxShadow={'lg'}
+          w={"full"}
+          maxW={"md"}
+          bg="white"
+          rounded={"xl"}
+          boxShadow={"lg"}
           p={6}
-          my={12}>
-          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+          my={12}
+          align={'center'}
+        >
+          <Spinner size="xl"></Spinner>
+        </Stack>
+      </Flex>
+    );
+  }
+  if (error) {
+    return (
+      <Flex align={"center"} justify={"center"} bg="gray.200">
+        <Stack
+          spacing={4}
+          w={"full"}
+          maxW={"md"}
+          bg="white"
+          rounded={"xl"}
+          boxShadow={"lg"}
+          p={6}
+          my={12}
+        >
+          <Text>{error.message.slice(9)} Please try again</Text>
+          <Input
+              onChange={e => setEmail(e.target.value)}
+              placeholder="your-email@example.com"
+              _placeholder={{ color: "gray.500" }}
+              type="email"
+            />
+          <Button
+            onClick={async () => {
+              const success = await sendPasswordResetEmail(email);
+              if (success) {
+                setSentStatus(true)
+              }
+            }}
+            bg={"blue.400"}
+            color={"white"}
+            _hover={{
+              bg: "blue.500",
+            }}
+          >
+            Request Reset
+          </Button>
+        </Stack>
+      </Flex>
+    );
+  } else {
+    return (
+      <Flex align={"center"} justify={"center"} bg="gray.200">
+        <Stack
+          spacing={4}
+          w={"full"}
+          maxW={"md"}
+          bg="white"
+          rounded={"xl"}
+          boxShadow={"lg"}
+          p={6}
+          my={12}
+        >
+          <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
             Forgot your password?
           </Heading>
-          <Text
-            fontSize={{ base: 'sm', sm: 'md' }}
-            color={useColorModeValue('gray.800', 'gray.400')}>
+          <Text fontSize={{ base: "sm", sm: "md" }} color="gray.400">
             You&apos;ll get an email with a reset link
           </Text>
           <FormControl id="email">
             <Input
+              onChange={e => setEmail(e.target.value)}
               placeholder="your-email@example.com"
-              _placeholder={{ color: 'gray.500' }}
+              _placeholder={{ color: "gray.500" }}
               type="email"
             />
           </FormControl>
           <Stack spacing={6}>
             <Button
-              bg={'blue.400'}
-              color={'white'}
+              onClick={async () => {
+                const success = await sendPasswordResetEmail(email);
+                if (success) {
+                  setSentStatus(true)
+                }
+              }}
+              bg={"blue.400"}
+              color={"white"}
               _hover={{
-                bg: 'blue.500',
-              }}>
+                bg: "blue.500",
+              }}
+            >
               Request Reset
             </Button>
           </Stack>
@@ -54,3 +148,4 @@ import {
       </Flex>
     );
   }
+}
--- src/components/navigation/RightNav.js
+++ src/components/navigation/RightNav.js
@@ -2,13 +2,48 @@ import React from 'react';
 import { Flex, Button, ColorModeScript } from '@chakra-ui/react';
 import { useSelector, useDispatch } from 'react-redux';
 import { showModal, hideModal } from '../../features/modals/modalToggleSlice';
+import { auth } from '../../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Spinner, Text } from '@chakra-ui/react';
+import { signOut } from 'firebase/auth';
+
 
 export default function RightNav() {
   const dispatch = useDispatch()
-  return (
+  const [user, loading, error] = useAuthState(auth);
+
+  const logOut = async () => {
+    signOut(auth).then(() => {  
+    }).catch((error) => {
+    });
+  }
+
+  if (loading) {
+    return (
+      <div>
+      <Spinner size='md' />
+    </div>
+    );
+  }
+  if (error) {
+    return (
+      <></>
+    );
+  }
+  if (user) {
+    return (
+      <>
+      <Text>{user.email}</Text>
+      <Button onClick={logOut}>Log out</Button>
+      </>
+    );
+  }
+  else {
+    return (
     <Flex>
       <Button onClick={() => dispatch(showModal())}>Log in</Button>
       <Button onClick={() => dispatch(showModal())}>Sign Up</Button>
     </Flex>
-  )
+    );
+  }
 }
