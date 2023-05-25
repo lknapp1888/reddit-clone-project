602dcd1ab115eaa3fcba445ca94d3eb6ae99280f
Lewis Knapp <lknapp1888@gmail.com>
2023-05-25 22:31:42 +0100

    Implement sorting via state rather than firestore

--- src/components/community/leftSide/CommunityPosts.js
+++ src/components/community/leftSide/CommunityPosts.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Button, Flex, Text } from '@chakra-ui/react';
 import { TriangleUpIcon, TriangleDownIcon, ChatIcon} from '@chakra-ui/icons';
 import { CircleIcon } from "../../../chakra/circleIcon";
@@ -7,10 +7,10 @@ import { query, where, collection, orderBy, getDocs } from 'firebase/firestore';
 import { db } from '../../../config/firebase';
 
 export default function CommunityPosts({community, sortSetting}) {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const postsFetched = useRef(false);
 
   const getPosts = async (sortOption = 'top') => {
-    
     try {
       const postQuery = query(
         collection(db, 'posts'),
@@ -24,10 +24,32 @@ export default function CommunityPosts({community, sortSetting}) {
     }
   }
 
-
+// initial render gets posts directly from server and saves in state
   useEffect(() => {
+    if (postsFetched.current) return;
+    postsFetched.current = true;
     getPosts(sortSetting)
   }, [])
+
+  // whenever sortSetting changes from user input, the posts stored in state render
+  // as to reduce reads to firestore
+  useEffect(() => {
+    const postsCopy = posts;
+    if (sortSetting === 'top') {
+      //sort by upvotes
+      setPosts(postsCopy.sort((a, b) => { 
+        return a.voteStatus - b.voteStatus;
+    }))
+      return
+    }
+    else {
+      // sort by new
+      setPosts(postsCopy.sort((a, b) => { 
+        return a.postTime - b.postTime;
+    }))
+      return
+    }
+  }, [sortSetting])
 
   return (
     <Flex direction='column' gap='3'>
