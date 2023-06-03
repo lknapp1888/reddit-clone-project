6e502838220f18c8d70f53f27571cf914432478c
Lewis Knapp <lknapp1888@gmail.com>
2023-06-03 18:48:35 +0100

    Add post content retrieval to post page

--- src/components/posts/PagePostItem.js
+++ src/components/posts/PagePostItem.js
@@ -1,39 +1,52 @@
-import {React, useState} from "react";
-import { Flex, FormControl, FormLabel, Text, Textarea, Button, Input } from "@chakra-ui/react";
+import { React, useState } from "react";
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Text,
+  Textarea,
+  Button,
+  Input,
+} from "@chakra-ui/react";
 import { doc, addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../../config/firebase";
+import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
 
-export default function PagePostItem({user, community, postId}) {
-    const [text, setText] = useState('');
-    const [textError, setTextError] = useState("");
+export default function PagePostItem({ user, community, postId, postData }) {
+  const [text, setText] = useState("");
+  const [textError, setTextError] = useState("");
 
-    const submitComment = async () => {
-        if (text.length === 0) {
-            setTextError('Comment must contain at least one character')
-            return;
-          }
-
-        try {
-            await addDoc(collection(db, "comments"), {
-              text: text,
-              authorId: user?.uid,
-              authorDisplayName: user.email.split('@')[0],
-              communityId: community,
-              parentPostId: postId,
-              postTime: serverTimestamp(),
-            });
-            setText('')
-            setTextError('')
-          }
-          catch (e) {
-            setTextError(`Error submitting post: ", ${e}`)
-          }
+  const submitComment = async () => {
+    if (text.length === 0) {
+      setTextError("Comment must contain at least one character");
+      return;
     }
 
+    try {
+      await addDoc(collection(db, "comments"), {
+        text: text,
+        authorId: user?.uid,
+        authorDisplayName: user.email.split("@")[0],
+        communityId: community,
+        parentPostId: postId,
+        postTime: serverTimestamp(),
+      });
+      setText("");
+      setTextError("");
+    } catch (e) {
+      setTextError(`Error submitting post: ", ${e}`);
+    }
+  };
 
   return (
-    <Flex direction='column' width='75%' margin='6' gap='5'>
-        <Flex>Hello I am post content...........</Flex>
+    <Flex direction="column" width="75%" margin="6" gap="5">
+      <Flex direction='column'>
+        <Text fontSize='xl' fontWeight='bold'>{postData.title}</Text>
+        <Flex border="solid black 1px" direction='column'>
+          <Text>{postData.text}</Text>
+        </Flex>
+        <Text>Posted {formatDistanceToNowStrict(postData.postTime.toDate())} ago by {postData.authorDisplayName}</Text>
+      </Flex>
       <Flex direction="column" gap="10px">
         <FormControl>
           <FormLabel fontSize="xl">Posting as user</FormLabel>
@@ -41,15 +54,17 @@ export default function PagePostItem({user, community, postId}) {
             placeholder="optional"
             value={text}
             maxLength="20000"
-            onChange={(e) => {setText(e.target.value)}}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
           ></Textarea>
-          <Flex justify='space-between'>
-              <Text>{20000 - text.length} characters remaining</Text>
-              <Button width="min-content" onClick={submitComment}>
-                Submit Comment
-              </Button>
+          <Flex justify="space-between">
+            <Text>{20000 - text.length} characters remaining</Text>
+            <Button width="min-content" onClick={submitComment}>
+              Submit Comment
+            </Button>
           </Flex>
-          <Text color='red'>{textError}</Text>
+          <Text color="red">{textError}</Text>
         </FormControl>
       </Flex>
     </Flex>
--- src/components/posts/PostPage.js
+++ src/components/posts/PostPage.js
@@ -1,30 +1,56 @@
-import { Flex } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import PagePostItem from './PagePostItem';
-import CommentItem from './CommentItem';
+import { Flex } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PagePostItem from "./PagePostItem";
+import CommentItem from "./CommentItem";
+import { doc, getDoc} from "firebase/firestore";
+import { db } from "../../config/firebase";
 
-export default function PostPage({community, user}) {
-    const postId = useParams().postId;
-    const [postData, setPostData] = useState({})
-    const [comments, setComments] = useState([])
+export default function PostPage({ community, user }) {
+  const postId = useParams().postId;
+  const [postData, setPostData] = useState({});
+  const [postExist, setPostExist] = useState(false)
+  const [comments, setComments] = useState([]);
 
-    useEffect(() => {
-      //check if the post exists, return error if not
-    }, [])
+  useEffect(() => {
+    getPost()
 
-    const getPost = async () => {
-      
+  }, []);
+
+  const getPost = async () => {
+    const docRef = doc(db, "posts", postId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists() && docSnap.data().communityId === community) {
+      setPostData(docSnap.data())
+      setPostExist(true)
+    } else {
+      console.log("No such document!");
     }
+  };
 
-    const getComments = async () => {
-
-    }
-
+  const getComments = async () => {};
+ 
+  if (postExist) {
+  return (
+    <Flex width="100%" direction="column" bg="white" height="400px">
+      <PagePostItem
+        postData={postData}
+        postId={postId}
+        user={user}
+        community={community}
+      ></PagePostItem>
+      {comments.map((c) => (
+        <CommentItem></CommentItem>
+      ))}
+    </Flex>
+  );
+  }
+  else {
     return (
-        <Flex width='100%' direction='column' bg='white' height='400px'>
-          <PagePostItem postData={postData} user={user} community={community} postId={postId}></PagePostItem>
-          {comments.map(c => <CommentItem></CommentItem>)}
-        </ Flex>
-      )
+      <Flex>
+        No such comment
+      </Flex>
+    )
+  }
 }
